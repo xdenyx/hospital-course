0252e7bef81ace4c913a47a8044e5616e8ffc079
t_denys <dtereta@gmail.com>
2026-05-23 14:04:04 +0300

    data check

--- hospital/services/ClinicService.py
+++ hospital/services/ClinicService.py
@@ -9,4 +9,6 @@ class ClinicService:
         """Вибірка: Список пацієнтів, за якими виконувалися роботи за період"""
         if not (start_date and end_date and work_category_id):
             return None
+        if start_date > end_date:
+            raise ValueError('Дата початку не може бути пізніше дати кінця.')
         return self.patient_repo.find_by_period_and_work(start_date, end_date, work_category_id)
