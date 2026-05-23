435db857ba7a16130caf06055b06b105eb0b7485
t_denys <dtereta@gmail.com>
2026-05-23 16:07:26 +0300

    docker

--- Dockerfile
+++ Dockerfile
@@ -11,11 +11,14 @@ WORKDIR /app
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Копируем весь код backend-приложения
+# Копируем весь код
 COPY . /app/
+
+# Собираем всю статику (HTML, CSS, JS) в одну папку для Whitenoise
+RUN python manage.py collectstatic --noinput
 
 # Открываем стандартный веб-порт
 EXPOSE 8000
 
-# Запускаем миграции и сам backend-сервер
+# Запускаем миграции, создаем админа (если его еще нет) и запускаем сервер
 CMD ["sh", "-c", "python manage.py migrate && python manage.py createsuperuser --noinput || true && gunicorn hospital_course.wsgi:application --bind 0.0.0.0:8000"]
--- hospital_course/settings.py
+++ hospital_course/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
@@ -140,9 +140,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
